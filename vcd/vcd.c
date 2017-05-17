c86ebb8371667d1f8d99d831a39efecba4571f7e
Marc Schott <marc.schott94@gmail.com>
2017-05-17 16:58:16 +0200

    Hinzufügen einer Option zur Wahl zwischen der nicht optimierten und optimierten Version des sequenziellen vcd-Algorithmus

--- vcd/vcd.c
+++ vcd/vcd.c
@@ -82,20 +82,25 @@ static double seconds() {
 
 int main(int argc, char* argv[]) {
 	bool execute_vcd  = false;
+	bool fast_vcd = false;
 	bool execute_sobel = false;
 	int option, rows, cols, maxval;
 	enum pnm_kind kind;
 	char* output_file = "out.pgm";
 	char* input_file;
 	uint8_t* picture;
-    while ((option = getopt(argc,argv,"hvso:")) != -1) {
+
+    while ((option = getopt(argc,argv,"hvfso:")) != -1) {
         switch(option) {
         	case 'h':
-        		printf("[-hvs][-o name_of_output_file] name_of_input_file\nThis program takes a picture in pgm format and processes it based on the given options.\nUse \"-h\" to display this description.\nUse \"-v\" to let the picture be manipulated by the vcd algortihm.\nUse \"-s\" to let the picture be manipulated by the sobel algortihm.\nIf both the \"-v\" and the \"-s\" options are set, the vcd algorithm will be executed first.\nUse \"-o\" to specify the file the processed image should be saved to. The default option ist \"out.pgm\".\nThe input file has to be given as the last argument.\n");
+        		printf("[-hvfs][-o name_of_output_file] name_of_input_file\nThis program takes a picture in pgm format and processes it based on the given options.\nUse \"-h\" to display this description.\nUse \"-v\" to let the picture be manipulated by the vcd algortihm.\nUse \"-f\" if you want the program to use the faster version of the vcd algortihm if \"-v\" is set, too.\nUse \"-s\" to let the picture be manipulated by the sobel algortihm.\nIf both the \"-v\" and the \"-s\" options are set, the vcd algorithm will be executed first.\nUse \"-o\" to specify the file the processed image should be saved to. The default option ist \"out.pgm\".\nThe input file has to be given as the last argument.\n");
         		return 0;
         	break;
         	case 'v':
         		execute_vcd = true;
+        	break;
+        	case 'f':
+        		fast_vcd = true;
         	break;
         	case 's':
         		execute_sobel = true;
@@ -139,8 +144,12 @@ int main(int argc, char* argv[]) {
 	double *image = convertImageToDouble(picture, rows, cols, maxval);
 	vcdNaive(image, rows, cols);
 
-	if(execute_vcd) {
-		// execute vcd algorithm
+	if(execute_vcd && !fast_vcd) {
+		// execute sequential, non-optimised vcd algorithm
+	}
+
+	if(execute_vcd && fast_vcd) {
+		// execute sequential, optimised vcd algorithm
 	}
 
 	if(execute_sobel) {
